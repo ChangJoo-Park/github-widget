88ea6e7e901857001065d6a90568adde5b9ada3e
ChangJoo Park <pcjpcj2@gmail.com>
2019-07-21 21:27:02 +0900

    Initial Commit

---
+++ github-widget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  github-widget
+//
+//  Created by ChangJoo Park on 21/07/2019.
+//  Copyright © 2019 ChangJoo Park. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
